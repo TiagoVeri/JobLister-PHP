7fd17163e2763964a9d40682f37e5a76ef6195f2
TiagoAlessandro <tiago.veri@mult-e.com.br>
2019-11-12 11:03:46 -0300

    first commit and project structure and templates added

---
+++ templates/inc/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JobLister</title>
+	<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	HEADER 
+
